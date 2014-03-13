1ea6bb031b2f398f3767a889cf9b603a33d8bdf3
Ashish Kumar <ashish009@live.com>
2014-03-14 00:37:45 +0530

    OLEDB project added rough commit

---
+++ OleDBApplication/OleDBApplication/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.OleDb;
+
+namespace WindowsFormsApplication1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            string ConnectionString = "Provider: Microsoft.Jet.OLEDB.4.0; Data Source='Abc'";
+            string abc = "Select * from emp";
+            using (OleDbConnection connection = new OleDbConnection(abc))
+            {
+                OleDbCommand command = new OleDbCommand(abc, connection);
+                try
+                {
+                    connection.Open();
+                    OleDbDataReader= command.ExecuteReader();
+                  
+
+                }
+            }
+        }
+    }
+}
